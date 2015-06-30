61b3f1bf431b17294bd4b85e77cab5e4fecf22b6
iebo <iebo8359@gmail.com>
2015-06-30 10:07:38 +0200

    added money that increases with 1 kr per person every 5 seconds

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-var populationCount = 0;
+var populationCount = 1;
 var clickIncrease = 1;
+var taxMoney = 0;
+var taxPerPop = 1;
 function showPopulation(){
-	document.getElementById("population").innerHTML = populationCount;
+	document.getElementById("population").innerHTML = populationCount + 1;
 	return populationCount;
+	taxes();
 
 };
+function taxes(){
+	setInterval(function(){ 
+  		taxMoney += populationCount * taxPerPop;
+		document.getElementById("money").innerHTML = taxMoney + "kr";
+}, 5000);
+}
